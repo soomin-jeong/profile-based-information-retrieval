39c472034897be526e9817d9523905d7b48e2ad9
Soo Min Jeong <soominjeongkr@gmail.com>
2021-04-07 21:54:23 +0200

    made an ml factory to collect all machine learning models

--- search_engine/ml_models.py
+++ search_engine/ml_models.py
@@ -12,22 +12,28 @@ from sklearn.model_selection import GridSearchCV
 
 
 class MachineLearningModel:
-    def __init__(self):
+    def __init__(self, model_name: str, pipeline: Pipeline):
         self.trained_model = None
         self.model_fp = os.path.join('models', self.model_name)
-
-    @property
-    def model_name(self):
-        raise NotImplementedError("Subclass should implement this")
+        self.model_name = model_name
+        self.pipeline = pipeline
 
     def load_model(self):
         return pickle.load(open(self.model_fp, 'rb'))
 
-    def train_model(self, x_train_, y_train, x_test, y_test):
-        raise NotImplementedError("Subclass should implement this")
+    def train_model(self, x_train, x_test, y_train, y_test):
+        model = self.pipeline
+        model.fit(x_train, y_train)
+        self.trained_model = model
+        predicted = model.predict(x_test)
+        accuracy = np.mean(predicted == y_test)
+        print("[{}] Accuracy: {}".format(self.model_name, accuracy))
+        self.save_model(model)
+        return model
 
     def predict(self, doc):
-        raise NotImplementedError("Subclass should implement this")
+        model = self.trained_model or self.load_model()
+        return model.predict([doc])
 
     def get_trained_model(self, x_train, y_train, x_test, y_test):
         try:
@@ -42,101 +48,44 @@ class MachineLearningModel:
             pickle.dump(model, f)
 
 
-class NaiveBayesClassifier(MachineLearningModel):
-    @property
-    def model_name(self):
-        return 'NaiveBayes'
+naive_bayes_pipeline = Pipeline([('count_vec', CountVectorizer()),
+                                 ('tfidf', TfidfTransformer()),
+                                 ('nb', MultinomialNB(fit_prior=False))])
+NaiveBayesClassifier = MachineLearningModel('NaiveBayes', naive_bayes_pipeline)
 
-    def train_model(self, x_train, x_test, y_train, y_test):
-        model = Pipeline([('count_vec', CountVectorizer()),
-                          ('tfidf', TfidfTransformer()),
-                          ('nb', MultinomialNB(fit_prior=False))])
+svm_pipeline = Pipeline([('count_vec', CountVectorizer()),
+                         ('tfidf', TfidfTransformer()),
+                         ('svm', SGDClassifier(random_state=1))])
+SVMClassifier = MachineLearningModel('SVM', svm_pipeline)
 
-        model.fit(x_train, y_train)
-
-        self.trained_model = model
-        self.save_model(model)
-
-        predicted = model.predict(x_test)
-        accuracy = np.mean(predicted == y_test)
-        print("\n[{}] Accuracy: {}".format(self.model_name, accuracy))
-        return model
-
-    def predict(self, doc):
-        model = self.trained_model or self.load_model()
-        return model.predict([doc])
-
-
-class SVMClassifier(MachineLearningModel):
-    @property
-    def model_name(self):
-        return 'SVM'
-
-    def train_model(self, x_train, x_test, y_train, y_test):
-        model = Pipeline([('count_vec', CountVectorizer()),
-                          ('tfidf', TfidfTransformer()),
-                          ('svm', SGDClassifier(random_state=1))])
-
-        model.fit(x_train, y_train)
-        self.trained_model = model
-
-        predicted = model.predict(x_test)
-        accuracy = np.mean(predicted == y_test)
-        print("[{}] Accuracy: {}".format(self.model_name, accuracy))
-        self.save_model(model)
-        return model
-
-    def predict(self, doc):
-        model = self.trained_model or self.load_model()
-        return model.predict([doc])
-
-
-class DecisionTree(MachineLearningModel):
-
-    @property
-    def model_name(self):
-        return 'DecisionTree'
-
-    def train_model(self, x_train, x_test, y_train, y_test):
-        model = Pipeline([('count_vec', CountVectorizer()),
+dt_pipeline = Pipeline([('count_vec', CountVectorizer()),
                           ('tfidf', TfidfTransformer()),
                           ('dt', DecisionTreeClassifier(max_depth=8, random_state=4))])
+DecisionTree = MachineLearningModel('DecisionTree', dt_pipeline)
 
-        model.fit(x_train, y_train)
-        self.trained_model = model
-
-        predicted = model.predict(x_test)
-        accuracy = np.mean(predicted == y_test)
-        print("[{}] Accuracy: {}".format(self.model_name, accuracy))
-        self.save_model(model)
-        return model
-
-    def predict(self, doc):
-        model = self.trained_model or self.load_model()
-        return model.predict([doc])
-
-
-class RandomForest(MachineLearningModel):
-
-    @property
-    def model_name(self):
-        return 'RandomForest'
-
-    def train_model(self, x_train, x_test, y_train, y_test):
-        model = Pipeline([('count_vec', CountVectorizer()),
+rf_pipeline = Pipeline([('count_vec', CountVectorizer()),
                           ('tfidf', TfidfTransformer()),
                           ('rf', RandomForestClassifier(max_depth=10, random_state=4))])
+RandomForest = MachineLearningModel('RandomForest', rf_pipeline)
 
-        model.fit(x_train, y_train)
-        self.trained_model = model
 
-        predicted = model.predict(x_test)
-        accuracy = np.mean(predicted == y_test)
-        print("[{}] Accuracy: {}".format(self.model_name, accuracy))
-        self.save_model(model)
-        return model
+class MachineLearningModelFactory:
+    def __init__(self, ml_models):
+        self.ml_models = ml_models
+
+    def train_model(self, x_train, x_test, y_train, y_test):
+        for each in self.ml_models:
+            each.train_model(x_train, x_test, y_train, y_test)
 
     def predict(self, doc):
-        model = self.trained_model or self.load_model()
-        return model.predict([doc])
+        preds = []
+        for each in self.ml_models:
+            each_pred = each.predict(doc)
+            preds.append(each_pred)
+        return max(preds, key=preds.count)
 
+
+ml_factory = MachineLearningModelFactory([NaiveBayesClassifier,
+                                          SVMClassifier,
+                                          DecisionTree,
+                                          RandomForest])
