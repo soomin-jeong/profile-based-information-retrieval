3d19b4ef9039b460cc11df360879ca58be83c5bc
Soo Min Jeong <soominjeongkr@gmail.com>
2021-04-08 19:04:37 +0200

    updated the readme with the installation and implementation options

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Information retrieval system based on a personal profile (preference)
 
 # Start by running
     python main.py
+
+# Refer to options
+    usage: main.py [-h] [--rebuild REBUILD] [--retrain RETRAIN]
+                   [--newuser NEWUSER]
+    optional arguments:
+      -h, --help         show this help message and exit
+      --rebuild REBUILD  It scraps the dataset and retrains the machine learning
+                         models.
+      --retrain RETRAIN  It retrains the machine learning models.
+      --newuser NEWUSER  You can add a new user and new interests.
+
--- search_engine/main.py
+++ search_engine/main.py
@@ -1,5 +1,6 @@
 import sys
 import uuid
+import argparse
 
 from document_retriever import DocumentRetriever
 from data_builder import DataBuilder
@@ -11,27 +12,31 @@ def main():
     dt = DataTrainer()
     db = DataBuilder()
     dr = DocumentRetriever()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rebuild", help="It scraps the dataset and retrains the machine learning models.")
+    parser.add_argument("--retrain", help="It retrains the machine learning models.")
+    parser.add_argument("--newuser", help="You can add a new user and new interests.")
+    args = parser.parse_args()
 
     with open('welcome', 'r') as f:
         msg = f.read()
         print(msg)
 
     # to crawl documents about the topics in user profiles
-    if len(sys.argv) > 1:
-        if '--rebuild' in sys.argv:
-            db.build_training_data()
-            dt.train_documents()
+    if args.rebuild:
+        db.build_training_data()
+        dt.train_documents()
 
-        if '--retrain' in sys.argv:
-            dt.train_documents()
+    if args.retrain:
+        dt.train_documents()
 
-        if '--new-user' in sys.argv:
-            interests = input("Enter your interests. Please divide by comma(,). :")
-            interests = [x.strip() for x in interests.split(',')]
-            db.build_training_data(new_topics=interests)
-            dt.train_documents()
-            new_profile = Profile(name='user{}'.format(uuid.uuid4()), interest=interests)
-            interest_integrater.insert_profile(new_profile)
+    if args.newuser:
+        interests = input("Enter your interests. Please divide by comma(,). :")
+        interests = [x.strip() for x in interests.split(',')]
+        db.build_training_data(new_topics=interests)
+        dt.train_documents()
+        new_profile = Profile(name='user{}'.format(uuid.uuid4()), interest=interests)
+        interest_integrater.insert_profile(new_profile)
 
     doc = input("Enter a document to match users' interests:")
 
